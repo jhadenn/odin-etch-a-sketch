cdb504a33e7609358394be0f4442a5b04648e7cb
jhaden goy <jhade@summrs.(none)>
2025-11-04 15:27:54 -0500

    completed basic etch-a-sketch function

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 
 const gridContainer = document.getElementById('grid-container')
 const gridSize = 16
+
+for(let i = 0; i < gridSize * gridSize; i++){
+
+    // create grid
+    const square = document.createElement('div');
+    square.classList.add('square');
+
+    // hover effect
+    square.addEventListener('mouseenter', () => {
+    square.classList.add('hovered')
+    });
+
+    gridContainer.appendChild(square)
+} 
+
+
